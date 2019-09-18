081b4ee75dd21d39f737271cfe88d90c3a6fba0b
lc <58054863@qq.com>
2019-09-18 08:59:39 +0800

    init pro

---
+++ SwiftUI Dome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Dome
+//
+//  Created by 罗超 on 2019/9/18.
+//  Copyright © 2019 罗超. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
